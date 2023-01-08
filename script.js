51b462ee5f795e57f9eab33d62ce9b8fd0f5507f
Krzysztof Rosa <krzysztof.rosa25@gmail.com>
2023-01-08 14:15:11 +0100

    ddalej nie wiem co robic

--- script.js
+++ script.js
@@ -17,4 +17,27 @@ function CPS(time) {
         }, time*1000);
     }
 button.innerHTML = clicks
+
+function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
+
+window.onload = () => {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    return width, height;
+}  
+window.onresize = () => {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    return width, height;
+}
+
+}
+(function(){
+    window.onmousemove = (e) => {
+    console.log(e.clientX, e.clientY);
+    document.querySelector("body").style.backgroundPositionX = map_range(e.clientX,0,);
+    document.querySelector("body").style.backgroundPositionY = e.clientY/100
+    }
+})()
