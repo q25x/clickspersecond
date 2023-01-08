be5df8b16faa06e67babfa21c7c8b98cbdcb1a18
Krzysztof Rosa <krzysztof.rosa25@gmail.com>
2023-01-08 17:16:51 +0100

    help mee

--- script.js
+++ script.js
@@ -33,30 +33,35 @@ window.onresize = () => {
     height = window.innerHeight;
     console.log(width, height);
 }
-
+document.querySelector("body").style.backgroundPositionX = "0px";
+document.querySelector("body").style.backgroundPositionY = "0px";
 (function(){
     window.onmousemove = (e) => {
-        //console.log(e.clientX, e.clientY);
-        // bgX = map_range(e.clientX,0,width,0,-300) + "px";
-        // bgY = map_range(e.clientY,0,height,0,-100) + "px";
         let bgX = parseInt(document.querySelector("body").style.backgroundPositionX.replace(/\D/g, ""));
         let bgY = parseInt(document.querySelector("body").style.backgroundPositionY.replace(/\D/g, ""));
+       
         let destination = {
-            x: map_range(e.clientX,0,width,0,-300),
-            y: map_range(e.clientY,0,height,0,-300)
+            x: Math.round(map_range(e.clientX,0,width,0,-300)),
+            y: Math.round(map_range(e.clientY,0,height,0,-300))
         };
-        console.log(bgX, bgY);
-        
-        switch(bgX,bgY) {
-            case bgX < destination.x:
-                bgX += (destination.x-bgX)/2
-            case bgX > destination.x:
-                bgX -= (bgX-destination.x)/2
-            case bgY < destination.y:
-                bgY += (destination.y-bgY)/2
-            case bgY > destination.y:
-                bgY -= (bgY-destination.y)/2
-        }
+       
+        console.log("bg: ", bgX, bgY);
+        console.log("dest: ", destination.x, destination.y);
+       
+        // switch(bgX,bgY) {
+        //     case bgX < destination.x:
+        //         bgX += (destination.x-bgX)/2
+        //     case bgX > destination.x:
+        //         bgX -= (bgX-destination.x)/2
+        //     case bgY < destination.y:
+        //         bgY += (destination.y-bgY)/2
+        //     case bgY > destination.y:
+        //         bgY -= (bgY-destination.y)/2       
+        // }      
+        if(bgX < destination.x) {bgX += (destination.x-bgX)}
+        if(bgX > destination.x) {bgX -= (bgX-destination.x)}
+        if(bgY < destination.y) {bgY += (destination.y-bgY)}
+        if(bgY > destination.y) {bgY -= (bgY-destination.y)}
         document.querySelector("body").style.backgroundPositionX = bgX + "px";
         document.querySelector("body").style.backgroundPositionY = bgY + "px";
 
